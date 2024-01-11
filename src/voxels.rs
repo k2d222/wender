02caefd6b3ca20582bf5a9139125bf5061eda1a1
Mathis Brossier <mathis.brossier@gmail.com>
2024-01-11 21:16:21 +0100

    compute svo (wip)

--- src/voxels.rs
+++ src/voxels.rs
@@ -1,52 +1,37 @@
 use nalgebra_glm as glm;
-use ndarray::{Array3, Axis};
+use ndarray::{s, Array3, ArrayView3, Axis, Zip};
 use rayon::prelude::{
     IndexedParallelIterator, IntoParallelIterator, ParallelBridge, ParallelIterator,
 };
 // use rayon::prelude::*;
 
+#[derive(Clone, Debug)]
 pub struct Voxels {
     voxels: Array3<u32>,
+    svo: Vec<SvoNode>,
     palette: Vec<glm::Vec4>,
-    pub dim: glm::TVec3<u32>,
 }
 // the octants are "pointers" (offsets in the contiguous memory buffer) to a next node.
 // at index 0 lies the root svo node.
 // if an octant pointer is 0, then it means empty (sparse).
-pub struct Svo1 {
+// octants are indexed in Array3's logical order, i.e. row major, i.e. z varies first.
+#[repr(C)]
+#[derive(Clone, Copy, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct SvoNode {
     octants: [u32; 8],
+}
+
+impl SvoNode {
+    fn is_empty(&self) -> bool {
+        self.octants.iter().all(|o| *o == 0)
+    }
+    fn is_full(&self) -> bool {
+        self.octants.iter().any(|o| *o != 0)
+    }
 }
 
 impl Voxels {
     pub fn new() -> Self {
-        // let dim = glm::vec3(512, 64, 512);
-
-        // println!("Generating voxels...");
-        // let voxels = (0..dim.product())
-        //     .into_par_iter()
-        //     .map(|i| {
-        //         let z = i % dim.z;
-        //         let y = (i / dim.z) % dim.y;
-        //         let x = i / (dim.z * dim.y);
-        //         let pos = glm::vec3(x as f32, y as f32, z as f32);
-        //         let pos_n = pos.component_div(&dim.cast());
-        //         let is_block =
-        //             pos_n.y < (pos_n.x * 32.93).cos() * (pos_n.z * 17.39).sin() * 0.5 + 0.5;
-        //         if is_block {
-        //             1
-        //         } else {
-        //             0
-        //         }
-        //     })
-        //     .collect();
-        // println!("done");
-
-        // let mut voxels = Array3::uninit((dim.x as usize, dim.y as usize, dim.z as usize))
-        //     .indexed_iter_mut()
-        //     .par_bridge()
-        //     .for_each(|(dim, mut row)| {
-        //     });
-
         let asset =
             dot_vox::load("assets/realistic_terrain.vox").expect("failed to load magicvoxel asset");
         let model = asset
@@ -54,10 +39,14 @@ impl Voxels {
             .get(0)
             .expect("expected 1 model in the asset file");
 
-        let dim = glm::vec3(model.size.x, model.size.y, model.size.z);
-        println!("dim: {dim}");
+        let dim = (
+            model.size.x as usize,
+            model.size.y as usize,
+            model.size.z as usize,
+        );
+        println!("dim: {dim:?}");
 
-        let mut voxels = Array3::zeros((dim.x as usize, dim.y as usize, dim.z as usize));
+        let mut voxels = Array3::zeros(dim);
         model.voxels.iter().for_each(|v| {
             voxels[(v.x as usize, v.z as usize, v.y as usize)] = v.i as u32 + 1;
         });
@@ -75,19 +64,91 @@ impl Voxels {
             })
             .collect();
 
+        let svo = Self::build_svo(&voxels);
+
         Self {
             voxels,
+            svo,
             palette,
-            dim,
         }
     }
 
-    // pub fn build_svo(&self) -> Svo {
-    //     let leaves = self
-    // }
+    pub fn build_svo(voxels: &Array3<u32>) -> Vec<SvoNode> {
+        fn leaf_svo(voxels: ArrayView3<u32>) -> SvoNode {
+            SvoNode {
+                octants: [
+                    voxels[(0, 0, 0)],
+                    voxels[(0, 0, 1)],
+                    voxels[(0, 1, 0)],
+                    voxels[(0, 1, 1)],
+                    voxels[(1, 0, 0)],
+                    voxels[(1, 0, 1)],
+                    voxels[(1, 1, 0)],
+                    voxels[(1, 1, 1)],
+                ],
+            }
+        }
+
+        fn node_svo(voxels: ArrayView3<SvoNode>) -> SvoNode {
+            SvoNode {
+                octants: [
+                    if voxels[(0, 0, 0)].is_empty() { 0 } else { 1 },
+                    if voxels[(0, 0, 1)].is_empty() { 0 } else { 1 },
+                    if voxels[(0, 1, 0)].is_empty() { 0 } else { 1 },
+                    if voxels[(0, 1, 1)].is_empty() { 0 } else { 1 },
+                    if voxels[(1, 0, 0)].is_empty() { 0 } else { 1 },
+                    if voxels[(1, 0, 1)].is_empty() { 0 } else { 1 },
+                    if voxels[(1, 1, 0)].is_empty() { 0 } else { 1 },
+                    if voxels[(1, 1, 1)].is_empty() { 0 } else { 1 },
+                ],
+            }
+        }
+
+        let l0 = Zip::from(voxels.exact_chunks((2, 2, 2))).par_map_collect(|o| leaf_svo(o));
+        let l1 = Zip::from(l0.exact_chunks((2, 2, 2))).par_map_collect(|o| node_svo(o));
+        let l2 = Zip::from(l1.exact_chunks((2, 2, 2))).par_map_collect(|o| node_svo(o));
+        let l3 = Zip::from(l2.exact_chunks((2, 2, 2))).par_map_collect(|o| node_svo(o));
+        let l4 = Zip::from(l3.exact_chunks((2, 2, 2))).par_map_collect(|o| node_svo(o));
+        let l5 = Zip::from(l4.exact_chunks((2, 2, 2))).par_map_collect(|o| node_svo(o));
+        let l6 = Zip::from(l5.exact_chunks((2, 2, 2))).par_map_collect(|o| node_svo(o));
+        let l7 = Zip::from(l6.exact_chunks((2, 2, 2))).par_map_collect(|o| node_svo(o));
+
+        let mut vec = Vec::new();
+        let mut ptr = 0u32;
+
+        let mut update_indices = |l: Array3<SvoNode>| {
+            // ptr += l.iter().filter(|o| o.is_full()).count() as u32;
+            l.into_iter().for_each(|mut o| {
+                if o.is_full() {
+                    o.octants.iter_mut().filter(|p| **p != 0).for_each(|p| {
+                        ptr += 1;
+                        *p = ptr;
+                    });
+                    vec.push(o);
+                }
+            });
+        };
+
+        update_indices(l7);
+        update_indices(l6);
+        update_indices(l5);
+        update_indices(l4);
+        update_indices(l3);
+        update_indices(l2);
+        update_indices(l1);
+        vec.extend(l0.into_iter());
+
+        println!("{} {}", vec.len(), ptr);
+
+        vec
+    }
 
     pub fn voxels_bytes(&self) -> &[u8] {
         bytemuck::cast_slice(self.voxels.as_slice().unwrap())
+    }
+
+    pub fn svo_bytes(&self) -> &[u8] {
+        bytemuck::cast_slice(self.svo.as_slice())
     }
 
     pub fn palette_bytes(&self) -> &[u8] {
